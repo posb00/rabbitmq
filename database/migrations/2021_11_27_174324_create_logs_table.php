0666177f8f03390c02296a9f415442dc13cff23b
Alexander <posb00@gmail.com>
2021-11-27 13:55:36 -0400

    create logs table

---
+++ database/migrations/2021_11_27_174324_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('symbol');
+            $table->decimal('open',18,2);
+            $table->decimal('high',18,2);
+            $table->decimal('low',18,2);
+            $table->decimal('close',18,2);
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
